2fff058cce76f1e94fe9fe72f1b0cd8fa7d779e5
Allen Kim <allenhwkim@gmail.com>
2016-07-27 09:41:05 -0400

    issue #2, enabling api key set to Ng2MapComponent

--- test/app/app.component.ts
+++ test/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, ViewChild, ViewContainerRef, ComponentResolver} from '@angular/core';
 import {Observable} from 'rxjs/Rx';
-import { NG2_MAP_DIRECTIVES } from "ng2-map";
+import { NG2_MAP_DIRECTIVES, Ng2MapComponent } from "ng2-map";
 
 @Component({
   selector: 'my-app',
@@ -12,7 +12,11 @@ export class AppComponent {
   public positions=[];
   @ViewChild('mapComponents', {read: ViewContainerRef}) mapComponents;
 
-  constructor(private compResolver:ComponentResolver) {}
+  constructor(
+    private compResolver:ComponentResolver
+  ) {
+    Ng2MapComponent.apiUrl = "https://maps.google.com/maps/api/js?key=AIzaSyCbMGRUwcqKjlYX4h4-P6t-xcDryRYLmCM";
+  }
 
   ngAfterViewInit(){
     let componentId = window.location.hash ? window.location.hash.replace(/#/,'') : 'simple-marker';
