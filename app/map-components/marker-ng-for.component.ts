1d16aef92637176c085bdaa33724d5e1b85de62c
Allen Kim <allenhwkim@gmail.com>
2016-12-24 09:18:51 -0500

    updated example

--- app/map-components/marker-ng-for.component.ts
+++ app/map-components/marker-ng-for.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/observable/of';
 
 let templateStr = `
   <h1>Marker Wigh *ngFor</h1>
   <ng2-map zoom="13" center="Brampton, Canada">
     <marker *ngFor="let pos of positions" [position]="pos"></marker>
   </ng2-map>
-  <button (click)="showRandomMarkers()">Show Random Markers</button>
+  <button (click)="positions = getRandomMarkers()">Show Random Markers</button> <br/>
+  <button (click)="showMarkersFromObservable()">Show Random Markers From Observable</button>
   <code>
    <br/><b>HTML</b>
     <pre>{{templateStr | htmlCode:'-code'}}</pre>
-    <b>function showRandomMarkers</b> 
-    <pre>{{showRandomMarkers | jsCode}}</pre>
+    <b>function getRandomMarkers</b> 
+    <pre>{{getRandomMarkers | jsCode}}</pre>
+    <b>function showMarkersFromObservable</b> 
+    <pre>{{showMarkersFromObservable | jsCode}}</pre>
   </code>
 `;
 @Component({
@@ -21,17 +26,25 @@ export class MarkerNgForComponent {
 
   templateStr: string = templateStr;
   constructor() {
-    this.showRandomMarkers();
+    this.positions = this.getRandomMarkers();
   }
 
-  showRandomMarkers() {
+  getRandomMarkers() {
     let randomLat: number, randomLng: number;
 
-    this.positions = [];
+    let positions = [];
     for (let i = 0 ; i < 9; i++) {
       randomLat = Math.random() * (43.7399 - 43.7300) + 43.7300;
       randomLng = Math.random() * (-79.7600 - -79.7699) + -79.7699;
-      this.positions.push([randomLat, randomLng]);
+      positions.push([randomLat, randomLng]);
     }
+    return positions;
+  }
+
+  showMarkersFromObservable() {
+    Observable.of(this.getRandomMarkers()) // Think this as http call
+      .subscribe( positions => {
+        this.positions = positions;
+      })
   }
 }
