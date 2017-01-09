fc32849f586d346c7804ea7cd0fc0b1ba7b46eb0
Avinash-Gupta-B <bommisettyavinasha@gmail.com>
2017-01-09 07:51:43 +0100

    Update directions-renderer.component.ts
    setPanel method was updated after line 67 to get view of directions panel

--- app/map-components/directions-renderer.component.ts
+++ app/map-components/directions-renderer.component.ts
@@ -65,6 +65,7 @@ export class DirectionsRendererComponent {
   ngOnInit() {
     this.directionsRendererDirective['initialized$'].subscribe( directionsRenderer => {
       this.directionsRenderer = directionsRenderer;
+      this.directionsRenderer.setPanel(document.querySelector('#my-panel'));
     });
   }
 
