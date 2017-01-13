f5f8c035ec3c31b50ddc7115de1f3a83b157d9a8
Ro Lewis <Rowan.Lewis@seeingmachines.com>
2017-01-13 12:40:18 +1100

    added custom-marker component

--- src/ng2-map.module.ts
+++ src/ng2-map.module.ts
@@ -7,30 +7,31 @@ import { NavigatorGeolocation } from './services/navigator-geolocation';
 
 import { Ng2MapComponent } from './components/ng2-map.component';
 import { InfoWindow } from './components/info-window';
+import { CustomMarker } from './components/custom-marker';
 
-import { BicyclingLayer } from "./directives/bicycling-layer";
+import { BicyclingLayer } from './directives/bicycling-layer';
 import { Circle } from './directives/circle';
-import { DataLayer } from "./directives/data-layer";
-import { DirectionsRenderer } from "./directives/directions-renderer";
-import { DrawingManager } from "./directives/drawing-manager";
+import { DataLayer } from './directives/data-layer';
+import { DirectionsRenderer } from './directives/directions-renderer';
+import { DrawingManager } from './directives/drawing-manager';
 import { GroundOverlay } from './directives/ground-overlay';
-import { HeatmapLayer } from "./directives/heatmap-layer";
-import { KmlLayer } from "./directives/kml-layer";
+import { HeatmapLayer } from './directives/heatmap-layer';
+import { KmlLayer } from './directives/kml-layer';
 import { Marker } from './directives/marker';
 import { Ng2Map } from './services/ng2-map';
-import { PlacesAutoComplete } from "./directives/places-auto-complete";
+import { PlacesAutoComplete } from './directives/places-auto-complete';
 import { Polygon } from './directives/polygon';
 import { Polyline } from './directives/polyline';
-import { StreetViewPanorama } from "./directives/street-view-panorama";
-import { TrafficLayer } from "./directives/traffic-layer";
-import { TransitLayer } from "./directives/transit-layer";
+import { StreetViewPanorama } from './directives/street-view-panorama';
+import { TrafficLayer } from './directives/traffic-layer';
+import { TransitLayer } from './directives/transit-layer';
 
 const COMPONENTS_DIRECTIVES = [
   Ng2MapComponent, InfoWindow,
-  Marker, Circle, Polygon, InfoWindow, Polyline, GroundOverlay,
+  Marker, Circle, CustomMarker, Polygon, InfoWindow, Polyline, GroundOverlay,
   TransitLayer, TrafficLayer, HeatmapLayer, BicyclingLayer, KmlLayer, DataLayer,
   StreetViewPanorama, PlacesAutoComplete, DirectionsRenderer,
-  DrawingManager
+  DrawingManager,
 ];
 
 @NgModule({
