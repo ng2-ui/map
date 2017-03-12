ef603b427c244d9d87d6d1e2d1206c5e3937e03d
Przemyslaw Olszewski <przem.olszewski@outlook.com>
2017-03-12 19:37:38 +0100

    Clear dist and ngc-pre-compiler add output func

--- dist/directives/traffic-layer.d.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from '@angular/core';
-import { BaseMapDirective } from './base-map-directive';
-import { Ng2MapComponent } from '../components/ng2-map.component';
-export declare class TrafficLayer extends BaseMapDirective {
-    autoRefresh: any;
-    options: any;
-    initialized$: EventEmitter<any>;
-    constructor(ng2MapComp: Ng2MapComponent);
-}
