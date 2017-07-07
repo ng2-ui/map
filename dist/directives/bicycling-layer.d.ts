b43112c02a75e3dcfbb8c69120059999271b1305
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-07-07 11:42:30 +0100

    add `dist` folder to .gitignore.

--- dist/directives/bicycling-layer.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseMapDirective } from './base-map-directive';
-import { NguiMapComponent } from '../components/ngui-map.component';
-export declare class BicyclingLayer extends BaseMapDirective {
-    constructor(nguiMapComp: NguiMapComponent);
-}
--- dist/services/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
-exports.NG_MAP_CONFIG_TOKEN = new core_1.OpaqueToken('NG_MAP_CONFIG_TOKEN');
-//# sourceMappingURL=config.js.map
