66660e99114d1abc39059bd5067125e5ce01e9b8
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-11-24 13:31:35 +0000

    feat(demo): auto deploy to gh-pages + use angular-cli

---
+++ app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { NguiUtilsModule } from '@ngui/utils';
+import { NguiMapModule } from '@ngui/map';
+import { SourceCodeService } from './source-code.service';
+
+import { AppComponent } from './app.component';
+// import { Codeblock } from 'ng2-prism/codeblock';
+
+import { APP_ROUTER_PROVIDERS, APP_ROUTER_COMPONENTS } from './app.route';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        APP_ROUTER_PROVIDERS,
+        // NguiMapModule,
+        NguiMapModule.forRoot({
+            apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyCbMGRUwcqKjlYX4h4-P6t-xcDryRYLmCM' +
+            '&libraries=visualization,places,drawing',
+        }),
+        NguiUtilsModule
+    ],
+    declarations: [AppComponent, APP_ROUTER_COMPONENTS],
+    providers: [
+        SourceCodeService,
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+    ],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
--- app/main.ts
+++ app/main.ts
@@ -1,45 +1,12 @@
-// polyfills, comment the following out for debugging purpose
-import 'core-js/es6';
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-
-// The browser platform with a compiler
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { AppModule } from './app.module';
+import { environment } from './environments/environment';
 
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { NguiUtilsModule } from '@ngui/utils';
-import { NguiMapModule } from '@ngui/map';
-import { SourceCodeService } from './source-code.service';
+if (environment.production) {
+  enableProdMode();
+}
 
-import { AppComponent } from './app.component';
-// import { Codeblock } from 'ng2-prism/codeblock';
-
-import { APP_ROUTER_PROVIDERS, APP_ROUTER_COMPONENTS } from './app.route';
-
-@NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    APP_ROUTER_PROVIDERS,
-    // NguiMapModule,
-    NguiMapModule.forRoot({
-      apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyCbMGRUwcqKjlYX4h4-P6t-xcDryRYLmCM' +
-      '&libraries=visualization,places,drawing',
-    }),
-    NguiUtilsModule ],
-  declarations: [AppComponent, APP_ROUTER_COMPONENTS],
-  providers: [
-    SourceCodeService,
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
-  ],
-  bootstrap: [ AppComponent ],
-})
-export class AppModule { }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
